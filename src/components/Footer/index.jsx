007391dbb31ad0168fef95b3fd1ec9cd7a91dba2
agiskorska <skorskaga@gmail.com>
2022-10-09 08:59:45 +0100

    Add some footer features

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
+import { Form } from '../index'
 import styles from './index.module.scss'
 function index() {
   return (
-    <footer className={styles.root}>Footer</footer>
+    <footer className={styles.root}>
+      <p>Created and designed by Aggie Skorska - Web Development © 2022</p>
+      <Form formId={'footerForm'} inputs={[{name:'contact', type:'text'}]}/>
+    </footer>
   )
 }
 
