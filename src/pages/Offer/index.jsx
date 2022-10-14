c534f9801c2d7b517f2f4018c863dca2af328869
agiskorska <skorskaga@gmail.com>
2022-10-14 05:25:18 +0100

    Create offer page

---
+++ src/pages/Offer/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import styles from './index.module.scss'
+import {OfferCard} from '../../components'
+
+function index() {
+  return (
+    <div>
+      <OfferCard />
+    </div>
+  )
+}
+
+export default index
