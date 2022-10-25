5a584d28b0a05342238db647203ae52fa91e4549
agiskorska <skorskaga@gmail.com>
2022-10-25 06:01:15 +0100

    Add links

--- src/components/Kaleidoscope/index.jsx
+++ src/components/Kaleidoscope/index.jsx
@@ -5,18 +5,18 @@ import styles from './index.module.scss'
 
 function index() {
   return (
-      <div className={styles.parent}>
-        <section className={styles.tiledGallery}>
-            <div className={styles.tile}><a className={styles.a} href="https://github.com/agiskorska">My Github</a></div>
-            <div className={styles.tile}><a href="https://www.linkedin.com/in/aggie-skorska-683924192/">LinkedIn</a></div>
-            <div className={styles.tile}>Fiverr</div>
-            <div className={styles.tile}>Chippenham Pride</div>
-            <div className={styles.tile}>Other Stuff</div>
-            <div className={styles.tile}>Small projects</div>
-            <div className={styles.tile}><a href="https://cohorts.getfutureproof.co.uk/available/aggieskorska">Futureproof</a></div>
-            <div className={styles.tile}>Call me</div>
-            <div className={styles.tile}>Email me</div>
-        </section>
+    <div className={styles.parent}>
+      <section className={styles.tiledGallery}>
+        <div className={styles.tile}><a className={styles.a} href="https://github.com/agiskorska">My Github</a></div>
+        <div className={styles.tile}><a href="https://www.linkedin.com/in/aggie-skorska-683924192/">LinkedIn</a></div>
+        <div className={styles.tile}>Fiverr</div>
+        <div className={styles.tile}>Chippenham Pride</div>
+        <div className={styles.tile}>Other Stuff</div>
+        <div className={styles.tile}>Small projects</div>
+        <div className={styles.tile}><a href="https://cohorts.getfutureproof.co.uk/available/aggieskorska">Futureproof</a></div>
+        <div className={styles.tile}>Call me</div>
+        <div className={styles.tile}>Email me</div>
+      </section>
     </div>
   )
 }
