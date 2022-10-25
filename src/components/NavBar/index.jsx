6533a4bdc71d3d1b66fdcafeefd8267012f1924e
agiskorska <skorskaga@gmail.com>
2022-10-25 07:01:57 +0100

    Fix footer at the bottom

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -11,7 +11,7 @@ function index() {
       <nav className={styles.navbar}>
         <NavLink className={styles.link} to='/'><h3 className={styles.h3}>Home</h3><h4 className={styles.h4}>See the main page</h4><h5  className={styles.h5}>for general stuff</h5></NavLink>
         <NavLink className={styles.link} to='/about'><h3 className={styles.h3}>About</h3><h4 className={styles.h4}>A little about me</h4><h5  className={styles.h5}>so we get familiar</h5></NavLink>
-        <NavLink className={styles.link} to='/work'><h3 className={styles.h3}>My Work</h3><h4 className={styles.h4}>See some of my work</h4><h5  className={styles.h5}>hope you enjoy it</h5></NavLink>
+        <NavLink className={styles.link} to='/work'><h3 className={styles.h3}>Offer</h3><h4 className={styles.h4}>See some of my work</h4><h5  className={styles.h5}>hope you enjoy it</h5></NavLink>
         <NavLink className={styles.link} to='/contact'><h3 className={styles.h3}>Contact</h3><h4 className={styles.h4}>Call me or email</h4><h5  className={styles.h5}>I like both!</h5></NavLink>
       </nav>
     </header>
